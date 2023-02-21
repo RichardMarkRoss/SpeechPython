638bc9ca2cb2978f4177d57804f71bd3218e4df8
RichardMarkRoss <cptrichkidprice@gmail.com>
2023-02-21 16:59:24 +0200

    fixes

--- main.py
+++ main.py
@@ -2,63 +2,64 @@ import sys
 import threading
 import tkinter as tk
 
-import pyttsx3 as tts
 import speech_recognition as sr
+import pyttsx3 as tts
 
 from  neuralintents import GenericAssistant
 
+
 class Assistant:
     
-    def __init__(self) -> None:
-        self.recognition = sr.Recognizer()
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
         self.speaker = tts.init()
         self.speaker.setProperty("rate", 150)
         
-        self.assistant = GenericAssistant("intent.json", intent_methods={"file": self.create_file})
-        self.assistant.train_modal()
+        self.assistant = GenericAssistant("intents.json", intent_methods={"file": self.create_file})
+        self.assistant.train_model()
         
-        self.root = tk.Tk
+        self.root = tk.Tk()
         self.label = tk.Label(text="🤖", font=("Arial", 120, "bold"))
         self.label.pack()
         
-        threading.Thread(target=self.run_assinstant).start()
+        threading.Thread(target=self.run_assistant).start()
         
         self.root.mainloop()
         
-        def create_file(self):
+    def create_file(self):
             with open("somefile.txt", "w") as f:
                 f.write("Hello World!")
         
-        def run_assistant(self):
-            while True:
-                try:
-                    with sr.Microphone() as mic:
-                        self.recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                        audio = self.recognizer.listen(mic)
+    def run_assistant(self):
+        while True:
+            try:
+                with sr.Microphone() as mic:
+                    self.recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                    audio = self.recognizer.listen(mic)
                         
+                    text = self.recognizer.recognize_google(audio)
+                    text = text.lower()
+                        
+                    if "hey bot" in text:
+                        self.label.config(fg="red")
+                        audio = self.recognizer.listen(mic)
                         text = self.recognizer.recognize_google(audio)
                         text = text.lower()
-                        
-                        if "hey bot" in text:
-                            self.label.config(fg="red")
-                            audio = self.recognizer.listen(mic)
-                            text = self.recognizer.recognize_google(audio)
-                            text = text.lower()
-                            if text == "stop":
-                                self.speaker.say("bye")
-                                self.speaker.runAndWait()
-                                self.speaker.stop()
-                                self.speaker.destroy()
-                                sys.exit()
-                            else:
-                                if text is not None:
-                                    response = self.assistant.requast(text)
-                                    if response is not None: 
-                                        self.speaker.say(response)
-                                        self.speaker.runAndWait()
-                                    self.label.config(fg="black")
-                except:
-                    self.label.config(fg="black")
-                    continue                
+                        if text == "stop":
+                            self.speaker.say("bye")
+                            self.speaker.runAndWait()
+                            self.speaker.stop()
+                            self.speaker.destroy()
+                            sys.exit()
+                        else:
+                            if text is not None:
+                                response = self.assistant.requast(text)
+                                if response is not None: 
+                                    self.speaker.say(response)
+                                    self.speaker.runAndWait()
+                                self.label.config(fg="black")
+            except:
+                self.label.config(fg="black")
+                continue                
                 
 Assistant()
